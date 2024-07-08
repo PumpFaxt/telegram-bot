eda2976c667c2d02caffddccf1f1304161b3a5f6
Riya Jain <riya1jain567@gmail.com>
2024-07-08 14:35:45 +0530

    Connect wallet

--- src/handlers/_index.ts
+++ src/handlers/_index.ts
@@ -1,4 +1,4 @@
-import { Message, Metadata } from "node-telegram-bot-api";
+import { Message, Metadata, SendMessageOptions } from "node-telegram-bot-api";
 import bot from "../../bot";
 import { generate256BitNonce } from "./register";
 
@@ -10,9 +10,21 @@ function handleMessage(message: Message, metadata: Metadata) {
   if (!rgr[chatId]) {
     const nonce = generate256BitNonce();
     rgr[chatId] = nonce;
-    bot.sendMessage(chatId, `Nonce generated: ${nonce}`);
+    const url = `https://github.com/jriyyya`;
+    const options: SendMessageOptions = {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Connect Wallet", url: url }]]
+      }
+    };
+    bot.sendMessage(chatId, `Nonce generated: ${nonce}\nClick the button below to open the website:`, options);
   } else {
-    bot.sendMessage(chatId, `Nonce already present: ${rgr[chatId]}`);
+    const url = `https://github.com/jriyyya`;
+    const options: SendMessageOptions = {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Connect Wallet", url: url }]]
+      }
+    };
+    bot.sendMessage(chatId, `Nonce already present: ${rgr[chatId]}\nClick the button below to open the website:`, options);
   }
 }
 
