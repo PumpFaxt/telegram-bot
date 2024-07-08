8a1fb85c204562a1d5b20a9d9c030f3b1296640a
Riya Jain <riya1jain567@gmail.com>
2024-07-08 13:48:18 +0530

    generate random nonce

--- src/handlers/_index.ts
+++ src/handlers/_index.ts
@@ -1,8 +1,19 @@
 import { Message, Metadata } from "node-telegram-bot-api";
 import bot from "../../bot";
+import { generate256BitNonce } from "./register";
+
+const rgr: Record<number, string> = {};
 
 function handleMessage(message: Message, metadata: Metadata) {
-    
+  const chatId = message.chat.id;
+
+  if (!rgr[chatId]) {
+    const nonce = generate256BitNonce();
+    rgr[chatId] = nonce;
+    bot.sendMessage(chatId, `Nonce generated: ${nonce}`);
+  } else {
+    bot.sendMessage(chatId, `Nonce already present: ${rgr[chatId]}`);
+  }
 }
 
-export default handleMessage
+export default handleMessage;
